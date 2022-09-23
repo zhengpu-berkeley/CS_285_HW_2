0d1f90fd5b4747fbc80f97bad8c9b3dbed768fbd
Zhengpu Zhao <zhengpuzhao@Zhengpus-MacBook-Air.local>
2022-09-23 16:59:57 -0700

    Brought in previous code from hw1

--- hw2/cs285/infrastructure/rl_trainer.py
+++ hw2/cs285/infrastructure/rl_trainer.py
@@ -159,9 +159,64 @@ class RL_Trainer(object):
 
     def collect_training_trajectories(self, itr, load_initial_expertdata, collect_policy, batch_size):
         # TODO: GETTHIS from HW1
+                # DONE decide whether to load training data or use the current policy to collect more data
+        # HINT: depending on if it's the first iteration or not, decide whether to either
+                # (1) load the data. In this case you can directly return as follows
+                # ``` return loaded_paths, 0, None ```
+
+                # (2) collect `self.params['batch_size']` transitions
+        if itr == 0:
+            # load expert data file with pickle
+            # here without a loop only first entry is loaded
+            with open(load_initial_expertdata, 'rb') as expert_data_file:
+                expert_data_first_entry = pickle.load(expert_data_file)
+            return expert_data_first_entry, 0, None
+
+
+        # DONE collect `batch_size` samples to be used for training
+        # HINT1: use sample_trajectories from utils
+        # HINT2: you want each of these collected rollouts to be of length self.params['ep_len']
+        print("\nCollecting data to be used for training...")
+        print('batch_size = {}'.format(batch_size))
+        n_paths = utils.sample_n_trajectories(
+            self.env, 
+            collect_policy, 
+            int(batch_size/self.params['ep_len']), 
+            self.params['ep_len'],
+            False) # render option assumed false, later the video collect will do it
+
+        envsteps_this_batch = 0
+        for path in n_paths:
+            envsteps_this_batch += path['observation'].shape[0]
+        print("envsteps_this_batch = {}".format(envsteps_this_batch))
+        # collect more rollouts with the same policy, to be saved as videos in tensorboard
+        # note: here, we collect MAX_NVIDEO rollouts, each of length MAX_VIDEO_LEN
+        train_video_paths = None
+        if self.log_video:
+            print('\nCollecting train rollouts to be used for saving videos...')
+            ## DONE look in utils and implement sample_n_trajectories
+            train_video_paths = utils.sample_n_trajectories(self.env, collect_policy, MAX_NVIDEO, MAX_VIDEO_LEN, True)
+
+        return n_paths, envsteps_this_batch, train_video_paths
 
     def train_agent(self):
-        # TODO: GETTHIS from HW1
+        print('\nTraining agent using sampled data from replay buffer...')
+        all_logs = []
+        for train_step in range(self.params['num_agent_train_steps_per_iter']):
+
+            # DONE sample some data from the data buffer
+            # HINT1: use the agent's sample function
+            # HINT2: how much data = self.params['train_batch_size']
+            ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch = self.agent.sample(
+                self.params['train_batch_size']
+            ) # this is random slices of (obs, acs, res, .., .., ..) from an agent rollout
+
+            # TODO use the sampled data to train an agent
+            # HINT: use the agent's train function
+            # HINT: keep the agent's training log for debugging
+            train_log = self.agent.train(ob_batch, ac_batch, re_batch, next_ob_batch, terminal_batch)
+            all_logs.append(train_log)
+        return all_logs
 
     ####################################
     ####################################
--- hw2/cs285/infrastructure/utils.py
+++ hw2/cs285/infrastructure/utils.py
@@ -55,13 +55,95 @@ def mean_squared_error(a, b):
 ############################################
 
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
-    # TODO: get this from hw1
+
+    # initialize env for the beginning of a new rollout
+    ob = env.reset() # HINT: should be the output of resetting the env
+
+    # init vars
+    obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
+    steps = 0
+    while True:
+
+        # render image of the simulated env
+        if render:
+            if hasattr(env, 'sim'):
+                image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
+            else:
+                image_obs.append(env.render())
+
+        # use the most recent ob to decide what to do
+        obs.append(ob)
+        ac = policy.get_action(ob) # HINT: query the policy's get_action function
+        ac = ac[0]
+        acs.append(ac)
+
+        # take that action and record results
+        ob, rew, done, _ = env.step(ac)
+
+        # record result of taking that action
+        steps += 1
+        next_obs.append(ob)
+        rewards.append(rew)
+
+        # DONE end the rollout if the rollout ended
+        # HINT: rollout can end due to done, or due to max_path_length
+        if (steps % 500) == 0:
+            print("steps = {}".format(steps))
+        
+        if done or (steps >= max_path_length):
+            rollout_done = True # HINT: this is either 0 or 1
+            print('steps = {} ; max_path_length = {}'.format(steps, max_path_length))
+            print('rollout is done')
+        else:
+            rollout_done = False
+
+        terminals.append(rollout_done)
+
+        if rollout_done:
+            break
+
+    return Path(obs, image_obs, acs, rewards, next_obs, terminals)
 
 def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
-    # TODO: get this from hw1
+    """
+        Collect rollouts until we have collected min_timesteps_per_batch steps.
+
+        DONE implement this function
+        Hint1: use sample_trajectory to get each path (i.e. rollout) that goes into paths
+        Hint2: use get_pathlength to count the timesteps collected in each path
+    """
+    # this is a single batch...
+    # while loop collects enough data for one batch 
+    # if data per rollout not enough.
+    timesteps_this_batch = 0
+    paths = [] # list to be filled with dictionaries
+    while timesteps_this_batch < min_timesteps_per_batch:
+
+        # curr_path_dict = {"observation" : np.array(obs, dtype=np.float32),
+                        # "image_obs" : np.array(image_obs, dtype=np.uint8),
+                        # "reward" : np.array(rewards, dtype=np.float32),
+                        # "action" : np.array(acs, dtype=np.float32),
+                        # "next_observation": np.array(next_obs, dtype=np.float32),
+                        # "terminal": np.array(terminals, dtype=np.float32)}
+        curr_path_dict = sample_trajectory(env, policy, max_path_length, render)
+        timesteps_this_batch += get_pathlength(curr_path_dict)
+        paths.append(curr_path_dict)
 
 def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, render_mode=('rgb_array')):
-    # TODO: get this from hw1
+    """
+        Collect ntraj rollouts.
+
+        DONE implement this function
+        Hint1: use sample_trajectory to get each path (i.e. rollout) that goes into paths
+    """
+    n_paths = [] # list to be filled with dictionaries
+
+    for i in range(ntraj):
+        print("sampling {}-th trajectory".format(i))
+        curr_path_dict = sample_trajectory(env, policy, max_path_length, render)
+        n_paths.append(curr_path_dict)
+
+    return n_paths
 
 ############################################
 ############################################
